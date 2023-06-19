956d50ecea1a6f013ffc606f60a7f2d1e6024217
RAKSHANDA-Achar <72595673+RAKSHANDA-Achar@users.noreply.github.com>
2023-06-19 22:59:49 +0530

    Update dockerrun.sh

--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -1,3 +1,3 @@
-sudo docker pull rakshandaj/raksha:Jenkinestest-v1
+sudo docker pull rakshandaj/raksha:javaapp-v1
 sudo docker rm -f Jenkinestest
-sudo docker run --name Jenkinestest -itd -p 8003:8080 rakshandaj/raksha:Jenkinestest-v1
+sudo docker run --name Jenkinestest -itd -p 8003:8080 rakshandaj/raksha:javaapp-v1
